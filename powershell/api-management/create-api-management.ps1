bb4997c03c76b93b8cf7c58f8173cad9f6e2f9ab
Fabrício Sanchez <fabsanc@microsoft.com>
2018-09-04 20:25:26 -0400

    Adjusting patch once again

---
+++ powershell/api-management/create-api-management.ps1
@@ -0,0 +1,29 @@
+# Verifying if user is currently logged on Azure
+function Login
+{
+    $needLogin = $true
+    Try 
+    {
+        $content = Get-AzureRmContext
+        if ($content) 
+        {
+            $needLogin = ([string]::IsNullOrEmpty($content.Account))
+        } 
+    } 
+    Catch 
+    {
+        if ($_ -like "*Login-AzureRmAccount to login*") 
+        {
+            $needLogin = $true
+        } 
+        else 
+        {
+            throw
+        }
+    }
+
+    if ($needLogin)
+    {
+        Login-AzureRmAccount
+    }
+}
