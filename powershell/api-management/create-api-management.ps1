de10e99637ce9144002d661a2ee7acc69e2f900c
Fabrício Sanchez <fabsanc@microsoft.com>
2018-09-04 22:50:42 -0400

    Adding option to select subscriptionId on login

--- powershell/api-management/create-api-management.ps1
+++ powershell/api-management/create-api-management.ps1
@@ -1,4 +1,5 @@
-# Verifying if user is currently logged on Azure
+# Verifying if user is currently logged on Azure.
+# If it is not, then call Login-AzureRmAccount.
 function Login
 {
     $needLogin = $true
@@ -24,6 +25,38 @@ function Login
 
     if ($needLogin)
     {
-        Login-AzureRmAccount
+        # Authenticating user
+        Login-AzureRmAccount 
+
+        # Listing all subscriptions under the logged user
+        Get-AzureRmSubscription
+
+        # Requesting the desired SubscriptionId
+        $subscriptionId = Read-Host -Prompt "SubscriptionId (Guid) you'd like to set as default"
+        
+        # Setting the default account
+        $accountSetResult = Set-AzureRmContext -SubscriptionId $subscriptionId
+        
+        if($accountSetResult)
+        {
+            Write-Host "Done. The context is now set up to" $subscriptionId"."
+        }
+        else
+        {
+            throw
+        }
+    }
+    else
+    {
+        Write-Host "User already logged. Information about the tenant can be seen below:"
+        Write-Host "--------------------------------------------------------------------"
+        (Get-AzureRmContext).Subscription
     }
 }
+
+function Main
+{
+    Login
+}
+
+Main
