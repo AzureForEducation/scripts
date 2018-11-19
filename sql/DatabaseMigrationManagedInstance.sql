9dcf8f1f24e45138ca23bd94c9dd6d365de20223
Fabrício Sanchez <fabricio.sanchez@gmail.com>
2018-11-19 08:11:17 -0500

    Update DatabaseMigrationManagedInstance.sql

--- sql/DatabaseMigrationManagedInstance.sql
+++ sql/DatabaseMigrationManagedInstance.sql
@@ -4,19 +4,19 @@ CREATE CREDENTIAL [your-blob-address-including-container]
 WITH IDENTITY = 'SHARED ACCESS SIGNATURE', 
 SECRET = 'your-shared-access-string-with-no-?-at-the-begining'
 
--- RESTORING BD_EMEC DATABASE
+-- RESTORING DATABASE 1
 
 RESTORE FILELISTONLY FROM URL = 'https://{your-storage-account-name}.blob.core.windows.net/{your-container}/{your-bak-file}'
 
 RESTORE DATABASE [your-db-name-1] FROM URL = 'https://{your-storage-account-name}.blob.core.windows.net/{your-container}/{your-bak-file}' 
 
--- RESTORING BD_SIMULADOCUSTO DATABASE
+-- RESTORING DATABASE 2
 
 RESTORE FILELISTONLY FROM URL = 'https://{your-storage-account-name}.blob.core.windows.net/{your-container}/{your-bak-file}'
 
 RESTORE DATABASE [your-db-name-2] FROM URL = 'https://{your-storage-account-name}.blob.core.windows.net/{your-container}/{your-bak-file}'
 
--- RESTORING BD_SGC DATABASE
+-- RESTORING DATABASE 3
 
 RESTORE FILELISTONLY FROM URL = 'https://{your-storage-account-name}.blob.core.windows.net/{your-container}/{your-bak-file}'
 
