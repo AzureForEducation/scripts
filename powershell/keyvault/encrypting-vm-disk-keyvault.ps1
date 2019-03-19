b8e9c0706e16b502f51631903cc46da8691d4e5b
Fabricio Sanchez <fabricio.sanchez@gmail.com>
2019-03-19 16:33:41 -0400

    Adding powershell scripts to disk encryption

---
+++ powershell/keyvault/encrypting-vm-disk-keyvault.ps1
@@ -0,0 +1,23 @@
+﻿# Logging into Azure
+Connect-AzAccount
+
+# Defining global variables
+$keyvaultname = 'your-keyvault-name'
+$rgname = 'your-resource-group-name'
+$aadclientsecret = 'your-client-secret'
+$vmname = 'your-vm-name'
+$appid = 'your-app-id'
+
+# Getting KeyVault values
+$keyvault = Get-AzKeyVault -VaultName $keyvaultname -ResourceGroupName $rgname
+$keyvaulturi = $keyvault.VaultUri
+$keyvaultresourceid = $keyvault.ResourceId
+
+# Encrypting the SO Disk
+Set-AzVMDiskEncryptionExtension -ResourceGroupName $rgname -VMName $vmname -AadClientID $appid -AadClientSecret $aadclientsecret -DiskEncryptionKeyVaultUrl $keyvaulturi -DiskEncryptionKeyVaultId $keyvaultresourceid
+
+# Encrypting the Data disk
+Set-AzVMDiskEncryptionExtension -ResourceGroupName $rgname -VMName $vmname -AadClientID $appid -AadClientSecret $aadclientsecret -DiskEncryptionKeyVaultUrl $keyvaulturi -DiskEncryptionKeyVaultId $keyvaultresourceid -VolumeType Data
+
+# Getting the status of the encryption process
+Get-AzVMDiskEncryptionStatus -VMName $vmname -ResourceGroupName $rgname
---
+++ powershell/keyvault/keyvault-aadapp-serviceprincipal.ps1
@@ -0,0 +1,31 @@
+﻿# Logging into Azure
+Connect-AzAccount
+
+# Creating global variables
+$keyvaultname = 'your-key-vault-name'
+$rgname = 'your-resource-group-name'
+$location = 'your-location'
+$aadclientsecret = ConvertTo-SecureString 'your-password-plain-text' -AsPlainText -Force
+$appname = 'your-app-name'
+
+# Deploying Resource Group
+New-AzResourceGroup -Name $rgname -Location $location
+
+# Deploying a new Azure Key Vault
+New-AzKeyVault -Name $keyvaultname -ResourceGroupName $rgname -Location $location
+
+# Enabling disk encryption into the Key Vault
+Set-AzKeyVaultAccessPolicy -VaultName $keyvaultname -ResourceGroupName $rgname -EnabledForDiskEncryption
+
+# Registering the virtual app into Azure Active Directory
+$aadapp = New-AzADApplication -DisplayName $appname -HomePage 'your-url' -IdentifierUris 'your-uri' -Password $aadclientsecret
+
+# Getting the registed AppId
+$appid = $aadapp.ApplicationId
+
+# Creating a Service Principal for our application
+$aadserviceprincipal = New-AzADServicePrincipal -ApplicationId $appid
+
+# Allowing encryption and decryption processess for the app within the Key Vault
+Set-AzKeyVaultAccessPolicy -VaultName $keyvaultname -ServicePrincipalName $appid -PermissionsToKeys encrypt,decrypt -PermissionsToSecrets set
+
